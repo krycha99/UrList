d27a3f4fde6f80273c398336a36f4ce245e7e03b
Krystian Sudoł <45632898+krycha99@users.noreply.github.com>
2022-10-05 15:16:17 +0200

    func finished for sure

--- resources/js/ListButtons.vue
+++ resources/js/ListButtons.vue
@@ -1,19 +1,19 @@
 <template>
     
-    <button class="btn btn-secondary mt-2 mb-2 mr-2"  type="button" aria-haspopup="true" aria-expanded="false" >-</button>  
+    <button class="btn btn-secondary mt-2 mb-2 mr-2"  type="button" aria-haspopup="true" aria-expanded="false" @click="epDim" >-</button>  
                     <div class="d-flex mt-3 mr-1">
                         <div class="text-xl font-bold mb-4" v-text="epWText"></div>
                         <p>/</p>
                         <div class="text-xl font-bold mb-4" v-text="epText"></div>
                     </div>
-                <button class="btn btn-secondary mt-2 mb-2 mr-2"  type="button" aria-haspopup="true" aria-expanded="false" >+</button>  
+                <button class="btn btn-secondary mt-2 mb-2 mr-2"  type="button" aria-haspopup="true" aria-expanded="false" @click="epAdd" >+</button>  
                 <button class="btn btn-secondary dropdown-toggle mt-2 mb-2" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-text="lstatusText" >
                 </button><span class="caret"></span>
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                  <li><a class="dropdown-item" >Watching</a></li>
-                  <li><a class="dropdown-item" >Completed</a></li>
-                  <li><a class="dropdown-item"  >Drop</a></li>
-                  <li><a class="dropdown-item"  href="/user/urlist/{{ $movie->id }}/plan">Plan to watch</a></li>
+                  <li><a class="dropdown-item" @click="watchBtn">Watching</a></li>
+                  <li><a class="dropdown-item" @click="comBtn">Completed</a></li>
+                  <li><a class="dropdown-item"  @click="dropM">Drop</a></li>
+                  <li><a class="dropdown-item"  @click="PlanBtn">Plan to watch</a></li>
                 
                 </div>
             
@@ -34,6 +34,64 @@
         },
 
         methods: {
+            dropM(){
+              axios.post('/list/' + this.movieId + '/drop').then(response => {
+                    console.log(response.data);
+                    window.location.reload();
+                  })
+                
+                
+          },
+          watchBtn(){
+            axios.post('/list/' + this.movieId + '/watching').then(response => {
+                    console.log(response.data);
+                    window.location.reload();
+                  });
+          },
+
+          comBtn(){
+            axios.post('/list/' + this.movieId + '/completed').then(response => {
+                    console.log(response.data);
+                    window.location.reload();
+                  });
+          },
+
+          PlanBtn(){
+            axios.post('/list/' + this.movieId + '/plan').then(response => {
+                    console.log(response.data);
+                    window.location.reload();
+                  });
+          },
+          epAdd(){
+            if(parseInt(this.episodesWatched) < parseInt(this.episodes))
+            {
+              
+              axios.post('/list/' + this.movieId + '/ep').then(response => {
+                    console.log(response.data);
+                    window.location.reload();
+                  });
+            }else {
+                comBtn();
+              console.log(this.episodesWatched + ' ' + this.episodes);
+              console.log('tutaj schowac przycisk');
+            }
+          
+          },
+
+          epDim(){
+            if((parseInt(this.episodesWatched) > 0) && (parseInt(this.episodesWatched) <= parseInt(this.episodes)))
+            {
+              
+              axios.post('/list/' + this.movieId + '/epDim').then(response => {
+                    console.log(response.data);
+                    window.location.reload();
+                  });
+            }else {
+              console.log(this.episodesWatched + ' ' + this.episodes);
+              console.log('tutaj schowac przycisk');
+            }
+          
+          }
 
         },
 
